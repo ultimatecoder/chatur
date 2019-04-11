f2106933947e7e9db140ea3eaf1376a849de8af0
ultimatecoder <jaysinhp@gmail.com>
2019-04-11 06:29:32 +0530

    Add functional test case for User story 3.
    Story: I am a User. Assume there is a MongoDB running on 27017 port and
    Next Cloud service running on port 8080, then scan between 27010 and
    27030 should only detect MongoDB service.
    
    Adding functionl test case for User story -4
    
    Story: Assume CloudNative service and MongoDB service is running. Scan
    between ports 8050 and 9000 should detect only CloudNative service on port
    8080.

--- end-to-end-tests/tests/base.py
+++ end-to-end-tests/tests/base.py
@@ -5,6 +5,10 @@ import os
 import unittest
 
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions
+
 
 
 class TestCase(unittest.TestCase):
@@ -33,6 +37,82 @@ class TestCase(unittest.TestCase):
         """
         path = f"{self.domain}{relative_path}"
         return self.driver.get(path)
+
+    def port_scan(self, start, end, wait=120):
+        """Runs Port scanning
+
+        Use this method to fill the form with desired values and start the port
+        scanning process. Default wait time for scanning process to complete is
+        100 seconds. Override it if the process is expected to wait for longer
+        time.
+
+        The method is returned when the port scanning process is ended.  The
+        method will observe the "status" label HTML element and when it is
+        texted as "Finished", this method assumes the port scanning process has
+        ended.
+
+        Parameters:
+            start : Port value from which the tool will initiate a scan. It
+            should be between 0 to 65535.
+            end   : Port value to which the tool will scan the process. It
+            should be between 0 to 65535.
+            wait  : Expected wait time until the scanning process will end.
+        """
+        self.get("/")
+        start_port_element = self.driver.find_element_by_xpath(
+            '//*[@id="start-port"]'
+        )
+        end_port_element = self.driver.find_element_by_xpath(
+            '//*[@id="end-port"]'
+        )
+        start_stop_button = self.driver.find_element_by_xpath(
+            '//*[@id="btn-discover"]'
+        )
+
+        start_port_element.send_keys("27010")
+        end_port_element.send_keys("27030")
+        start_stop_button.click()
+
+        secounds = 120
+        wait = WebDriverWait(self.driver, secounds)
+        status = wait.until(
+            expected_conditions.text_to_be_present_in_element(
+                (By.ID, 'status'),
+                "Finished"
+            )
+        )
+
+    def assert_port_in_result(self, port, description):
+        """Asserts port and value in scanned result
+
+        Preferred method to call after calling `self.port_scan() method.
+
+        Parameters:
+            port        : Desired port value that should be in list of ports
+            detected.
+            description : Description value that should be in list of
+            description.
+        """
+        port_elements = self.driver.find_elements_by_xpath(
+            "/html/body/div[2]/div[3]/div/div/table/tbody/tr/td[1]"
+        )
+        description_elements = self.driver.find_elements_by_xpath(
+            "/html/body/div[2]/div[3]/div/div/table/tbody/tr/td[2]"
+        )
+
+        ports = list(map(lambda r: r.text, port_elements))
+        descriptions = list(map(lambda d: d.text, description_elements))
+
+        self.assertIn(
+            str(port),
+            ports,
+            "Desired port do not found in scanned result"
+        )
+        self.assertIn(
+            str(description),
+            descriptions,
+            "Desired description do not found in description result"
+        )
 
     def tearDown(self):
         self.driver.close()
